ccf94e05808b48fa5931646ec207abb9c31c4a49
JaredMZ <jaredcv04@gmail.com>
2024-01-23 12:03:38 -0600

    adding coins api

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -2,7 +2,7 @@ import useFetch from "../useFetch";
 
 function CoinsTable() {
   const { data, loading } = useFetch(
-    "https://jsonplaceholder.typicode.com/users"
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10"
   );
 
   return (
@@ -16,17 +16,20 @@ function CoinsTable() {
         <table>
           <thead>
             <tr>
-              <th>ID</th>
               <th>Name</th>
-              <th>Email</th>
+              <th>Symbol</th>
+              <th>Price</th>
+              <th>Market Cap %</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
             {data.map((coin) => (
               <tr key={coin.id}>
-                <td>{coin.id}</td>
-                <td>{coin.name}</td>
-                <td>{coin.email}</td>
+                <td> <img className="coin-logo" src={coin.image} alt="" /> {coin.name}</td>
+                <td>{coin.symbol}</td>
+                <td>{coin.current_price}</td>
+                <td>{coin.market_cap_change_percentage_24h}</td>
               </tr>
             ))}
           </tbody>
