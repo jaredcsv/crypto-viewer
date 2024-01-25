f19b2a2d8ef980395b4d3e13aea711ec57467060
JaredMZ <jaredcv04@gmail.com>
2024-01-25 15:48:01 -0600

    mobile menu bug fixed

--- src/components/MenuBarMobile.jsx
+++ src/components/MenuBarMobile.jsx
@@ -2,34 +2,35 @@ import "/src/styles/components/MenuBarMobile.css";
 
 function MenuBarMobile() {
   return (
-    <>
-      <nav>
-        <div className="navbar">
-          <div className="container nav-container">
-            <input className="checkbox" type="checkbox" name="" id="" />
-            <div className="hamburger-lines">
-              <span className="line line1"></span>
-              <span className="line line2"></span>
-              <span className="line line3"></span>
-            </div>
-            <div className="logo">
-              <img className="logo-img-mobile" src="/src/assets/img/wormcoins-logo.png" alt="" />
-            </div>
-            <div className="menu-items">
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">Services</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-            </div>
-          </div>
+<>
+  <nav>
+    <div className="navbar">
+      <div className="container nav-container">
+        <input className="checkbox" type="checkbox" name="" id="" />
+        <div className="hamburger-lines">
+          <span className="line line1"></span>
+          <span className="line line2"></span>
+          <span className="line line3"></span>
         </div>
-      </nav>
-    </>
+        <div className="logo">
+          <img className="logo-img-mobile" src="/src/assets/img/wormcoins-logo.png" alt="" />
+        </div>
+        <ul className="menu-items">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</>
+
   );
 }
 
